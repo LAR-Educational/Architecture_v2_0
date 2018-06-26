54c6a75ce073c344fcf02bb38be2dea0ec29db1a
dtozadore <dtozadore@gmail.com>
2018-06-26 14:50:41 -0300

    adaptive news

--- Arch_2_1/Modules/adaption.py
+++ Arch_2_1/Modules/adaption.py
@@ -9,12 +9,12 @@ import time
 import time
 import os
 
-def normalize(read_val, max_val, roof=1):
+def normalize(read_val, max_val, min_val=0, floor=-1, roof=1):
 	"""
 	Normalize two numbers betwenn 0 to 1
 	"""
-	return float((read_val*roof)/max_val)
-	
+	#return float((read_val*roof)/max_val)
+	return float ( (roof-floor)/(max_val-min_val)*(read_val-max_val)+ roof )
 
 
 
@@ -28,7 +28,7 @@ class OperationalParameters:
 	def __init__(self, max_deviation, max_emotion_count, 
 					min_number_word, max_time2ans, min_suc_rate	):
 					
-		self.max_deviation = max_deviation, 
+		self.max_deviation = max_deviation 
 		self.max_emotion_count = max_emotion_count  
 		self.min_number_word =  min_number_word
 		self.max_time2ans = max_time2ans
@@ -46,7 +46,7 @@ class ReadValues:
 	def __init__(self, deviations=5, emotionCount=3, 
 					numberWord=1, time2ans=20, sucRate=1		):
 					
-		self.deviations = deviations, 
+		self.deviations = deviations 
 		self.emotionCount = emotionCount  
 		self.numberWord =  numberWord
 		self.time2ans = time2ans
@@ -72,25 +72,23 @@ class AdaptiveSystem:
 		self.op = op
 		self.w = w #weights class
 		self.rv = rv
-		print rv.deviations
+		
 
-
-	def adp_function(self):
+	def adp_function(self, fadp_previous_value = 0):
 		
 		#calculating the alpha vector
-		print self.op.max_deviation
 		
 		alpha = normalize(self.rv.deviations, self.op.max_deviation)
 		core.info("Alpha :" + str(alpha)) 
 		
 		#calculating the beta vector
-		beta = normalize( (normalize(self.rv.emotion_count,self.op.max_emotion_count) 
-							+ normalize(self.rv.number_word,self.op.min_number_word) )/2)
+		beta = (normalize(self.rv.emotionCount, self.op.max_emotion_count) + 
+							 normalize(self.rv.numberWord, self.op.min_number_word) )/2
 		core.info("Beta :" + str(beta)) 
 		
 		#calculating the gama vector
-		gama = normalize( (normalize(self.rv.time2ans, self.op.max_time2ans) 
-							+ normalize(self.rv.suc_rate, self.op.min_suc_rate) )/2)
+		gama = (normalize(self.rv.time2ans, self.op.max_time2ans) 
+							+ normalize(self.rv.sucRate, self.op.min_suc_rate) )/2
 		core.info("Gama :" + str(gama)) 
 		
 		
@@ -98,9 +96,27 @@ class AdaptiveSystem:
 		core.info("fadp = w.alpha*alpha + w.beta*beta + w.gama*gama")
 		core.info(str(fadp) + " = " 
 		 			+ str(self.w.alpha) + "*" + str(alpha) 
-		 			+ "+" + str(self.w.beta) + "*" + str(beta) + " + "
+		 			+ " + " + str(self.w.beta) + "*" + str(beta) + " + "
 		 			+  str(self.w.gama) + "*" + str(gama))
-		 
+		
+		# fadp(t) = fadp(t-1) + fadp(t)
+		fadp =  fadp + fadp_previous_value
+		core.info("Final: " + str(fadp))				
+			
+		return fadp
+		
+		
+		
+	def activation_function(self, fadp):	
+		#Activation function
+				
+		if fadp > 0.65:
+			return 1
+		elif fadp < 0.33:	
+			return -1
+		else:
+			return 0	
+		
 		
 		
 		
@@ -109,28 +125,42 @@ class AdaptiveSystem:
 
 def main():
 
+	key = ""
+	fa = 0
 
-	#print "test", normalize(1.75,5)
+	#w = Weights(0.3, 0.2, 0.5 )	
+	w = Weights(0.3, 0.1, 0.2 )	
 
-	w = Weights(0.15, 0.05, 1.5 )	
 	op = OperationalParameters (max_deviation=5.0, max_emotion_count=3, 
-								min_number_word=1 , max_time2ans=20, min_suc_rate=1)
-	rv= ReadValues(deviations=5, emotionCount=3, 
-					numberWord=1, time2ans=20, sucRate=1	)
+									min_number_word=1 , max_time2ans=10, min_suc_rate=1)
+	
+	while(key!="e"):
+
+		#print "test", normalize(1.75,5)
 
 	
-	print "on main", op.max_deviation
-	print "weights", w.alpha
-	print "rv.deviations", rv.deviations
+		rv= ReadValues(deviations=random.randint(0, 5), emotionCount=random.randint(0, 3), 
+						numberWord=random.randint(0,1), time2ans=random.randint(0, 10), 						sucRate=random.randint(0, 1)	)
+
+	
+		#print "on main", op.max_deviation
+		#print "weights", w.alpha
+		#print "rv.deviations", rv.deviations
 	
 	
 	
-	adpt = AdaptiveSystem(robot=1,path=2, op=op, w=w, rv=rv)
+		adpt = AdaptiveSystem(robot=1,path=2, op=op, w=w, rv=rv)
 
+		fc = adpt.adp_function(fa)
+		core.info( "Fadp: " + str(fc) )
+		
+		
+		act = adpt.activation_function(fc)
+		core.info( "Activation: " + str(act) )
+		
+		key=raw_input("Key: ")
 
-	adpt.adp_function()
-
-
+		fa = fc
 
 
 if __name__ == "__main__":
