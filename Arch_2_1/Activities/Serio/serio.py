692ace975a570d1f111279e70d50cfb21ca6e67e
dtozadore <dtozadore@gmail.com>
2018-06-27 11:55:18 -0300

    nao sei

--- Arch_2_1/Activities/Serio/serio.py
+++ Arch_2_1/Activities/Serio/serio.py
@@ -8,11 +8,9 @@ from Modules.vars import Robot, robotIp, port, emotions, deviation_times
 robot = Robot(robotIp, port)
 
 attention = disattention.Th(1)
-attention.run()
+attention.start()
 
-animatedSpeech = naoqi.ALProxy("ALAnimatedSpeech", ip, port)
-speech = naoqi.ALProxy("ALTextToSpeech", ip, port)
-speech.setLanguage("Brazilian")
+robot.tts.setLanguage("Brazilian")
 
 with open('Activities/Serio/frases.txt', 'r') as arq:
 	frases = arq.read().split('\n')
@@ -23,27 +21,23 @@ for i, frase in enumerate(frases):
 	frase_dict[i] = frase
 
 r = random.sample(range(7), 3)
-print("aqui")
-speech.say("O jogo do sério é uma brincadeira onde eu e você iremos ficar sérios. Quem rir primeiro ou olhar para o lado perde. Chegue mais perto para que eu possa te ver. Vamos começar!")
 
-intial_time = time.time()
+robot.tts.say("O jogo do sério é uma brincadeira onde eu e você iremos ficar sérios. Quem rir primeiro ou olhar para o lado perde. Chegue mais perto para que eu possa te ver. Vamos começar!")
 
-while True:
-	if(initial_time - time.time() < 7):
-		animatedSpeech.say(frase_dict[r[0]])
-	elif(initial_time - time.time() < 20):
-		animatedSpeech.say(frase_dict[r[1]])
-	elif(initial_time - time.time() < 30):	
-		animatedSpeech.say(frase_dict[r[2]])
-	elif(initial_time - time.time() < 40):
-		break
+initial_time = time.time()
+
+time.sleep(5)
+
+for i in range(3):
+	robot.tts.say(frase_dict[r[i]])
+	time.sleep(8)
 
 
 print(emotions)
-print(deviations)
+print(deviation_times)
 
 closeAttention = disattention.Th(2)
-closeAttention.run()
+closeAttention.start()
 
 
 
