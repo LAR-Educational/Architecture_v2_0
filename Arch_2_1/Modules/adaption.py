644c9e6eed34fd69f6d34d25b3a96ae543ac7183
dtozadore <dtozadore@gmail.com>
2018-07-05 19:02:50 -0300

    writing logs

--- Arch_2_1/Modules/adaption.py
+++ Arch_2_1/Modules/adaption.py
@@ -9,9 +9,13 @@ import time
 import time
 import os
 
+
+
+
+
 def normalize(read_val, max_val, min_val=0, floor=0, roof=1):
 	"""
-	Normalize two numbers betwenn 0 to 1
+	Normalize two numbers betwenn floor to roof
 	"""
 	#return float((read_val*roof)/max_val)
 	return float ( (roof-floor)/(max_val-min_val)*(read_val-max_val)+ roof )
@@ -50,15 +54,14 @@ class AdaptiveSystem:
 
 	def __init__(self, op, w, rv):
 	
-	    #self.robot = robot
-	    #self.path = path
-	    self.op = op
-	    self.w = w #weights class
-	    self.rv = rv		
+	    	#self.robot = robot
+	    	#self.path = path
+	    	self.op = op
+	    	self.w = w #weights class
+		self.rv = rv		
+		flag_log=core.flag_log	
 
-	
-
-	def adp_function(self, fadp_previous_value = 0):
+	def adp_function(self, adaptive_frame, fadp_previous_value = 0):
 	
 		#calculating the alpha vector
 	
@@ -77,16 +80,31 @@ class AdaptiveSystem:
 	
 	
 		fadp = self.w.alpha*alpha + self.w.beta*beta + self.w.gama*gama
-		core.info("fadp = w.alpha*alpha + w.beta*beta + w.gama*gama")
+		core.info("fadp(t) = w.alpha*alpha + w.beta*beta + w.gama*gama")
 		core.info(str(fadp) + " = " 
-		 			+ str(self.w.alpha) + "*" + str(alpha) 
-		 			+ " + " + str(self.w.beta) + "*" + str(beta) + " + "
-		 			+  str(self.w.gama) + "*" + str(gama))
+					+ str(self.w.alpha) + "*" + str(alpha) 
+					+ " + " + str(self.w.beta) + "*" + str(beta) + " + "
+					+  str(self.w.gama) + "*" + str(gama))
 	
 		# fadp(t) = fadp(t-1) + fadp(t)
 		fadp =  fadp + fadp_previous_value
 		core.info("Final: " + str(fadp))				
 		
+		if core.flag_log:
+			filename = os.path.join("Log/AdaptiveLogs","vectors_int_" + str(core.interaction_id)+".dat")
+			#print filename		
+			#print "Log/AdaptiveLogs/vectors_int_" + str(core.interaction_id)+".dat"	
+
+			log_file = open(filename, "a+" )
+			log_file.write(str(adaptive_frame) 
+					+ "," + str(alpha) 
+					+ "," + str(beta) 
+					+ "," + str(gama)
+					+ "," + str(fadp)  
+					+ "," + str( self.activation_function(fadp) ) )
+			log_file.write("\n")
+			log_file.close()
+
 		return fadp
 	
 	
@@ -113,7 +131,7 @@ def change_behavior(self, robot, behavior):
 		robot.tts.setParameter('volume', robot.volume-0.2)
 		robot.tts.setParameter('speed',robot.speed+0.2)	
 	
-	 	
+		
 		
 		
 
@@ -128,24 +146,26 @@ def main():
 	op = OperationalParameters (max_deviation=5.0, max_emotion_count=3, 
 									min_number_word=1 , max_time2ans=10, min_suc_rate=1)
 	
+	adap_frame=0
+
 	while(key!="e"):
 
 		#print "test", normalize(1.75,5)
 
 	
 		rv= core.ReadValues(deviations=random.randint(0, 5), emotionCount=random.randint(0, 3), 
-						numberWord=random.randint(0,1), time2ans=random.randint(0, 10), 						sucRate=random.randint(0, 1)	)
+						numberWord=random.randint(0,1), time2ans=random.randint(0, 10),							sucRate=random.randint(0, 1)	)
 
 	
 		#print "on main", op.max_deviation
 		#print "weights", w.alpha
 		#print "rv.deviations", rv.deviations
-	
+			
 	
 	
 		adpt = AdaptiveSystem(op=op, w=w, rv=rv)
 
-		fc = adpt.adp_function()
+		fc = adpt.adp_function(adap_frame)
 		core.info( "Fadp: " + str(fc) )
 		
 		
@@ -155,7 +175,7 @@ def main():
 		key=raw_input("Key: ")
 
 		fa = fc
-
+		adap_frame +=1
 
 if __name__ == "__main__":
 	main()
